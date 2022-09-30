f12316d0b3088580c3fc40318a52c3b3a630b9ba
frezes <zhangjunhao@kubesphere.io>
2022-09-30 14:44:03 +0800

    add version info and optimize Makefile
    Signed-off-by: frezes <zhangjunhao@kubesphere.io>

--- pkg/api/monitoring/v1alpha1/groupversion_info.go
+++ pkg/api/monitoring/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the monitoring v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=monitoring.whizard.io
+// +kubebuilder:object:generate=true
+// +groupName=monitoring.whizard.io
 package v1alpha1
 
 import (
